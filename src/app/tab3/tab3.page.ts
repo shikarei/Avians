ec77603ea4fdd8666829dc459bb661779fe08acf
rei <atofu2510@gmail.com>
2025-09-06 10:59:45 +0700

    ionic-part2

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,8 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
+
 export class Tab3Page {
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: { action: 'delete' },
+      handler: () => {
+        console.log('Delete clicked');
+      }
+    },
+    {
+      text: 'Share',
+      data: { action: 'share' },
+      handler: () => {
+        console.log('Share clicked');
+      }
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: { action: 'cancel' },
+    },
+  ];
 
-  constructor() {}
-
+  constructor() { }
 }
